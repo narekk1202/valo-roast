e1b7af08794878acacfbd7e55e9471b8d72eb5d2
narekk1202 <narekdunamaliiian@gmail.com>
2026-08-19 13:30:14 +0400

    Update layout to suppress hydration warnings in HTML tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body
 				className={`${fontSans.variable} ${fontSerif.variable} ${fontMono.variable} antialiased`}
 			>
